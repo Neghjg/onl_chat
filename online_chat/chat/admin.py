dd0ac67250bbdec6243cfa680e27f796127118eb
Neghjg <neghjg@yandex.ru>
2024-03-20 18:22:53 +0300

    remove Chat and User Messegese

--- online_chat/chat/admin.py
+++ online_chat/chat/admin.py
@@ -1,18 +1,6 @@
 from django.contrib import admin
-from chat.models import ChatMessage2, UserMessege2, ChatMessage3, UserMessege3
+from chat.models import ChatMessage3, UserMessege3
 # Register your models here.
-
-
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user1', 'user2', 'created', 'updated')
-
-
-class UserMessegeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'chat_room', 'user', 'text')
-
-
-admin.site.register(ChatMessage2, ChatMessageAdmin)
-admin.site.register(UserMessege2, UserMessegeAdmin)
 
 
 class ChatMessageAdmin(admin.ModelAdmin):
